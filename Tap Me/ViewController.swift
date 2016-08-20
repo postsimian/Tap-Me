2a512d0fe113f9af582d053f62225f67947d7f5b
postsimia <postsimian@gmail.com>
2016-08-20 17:24:07 -0400

    My tap-me app first commit
    my first commit for tap me

--- Tap Me/ViewController.swift
+++ Tap Me/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    
+    var count = 0
+    
+    var seconds = 0
+    var timer = NSTimer()
+    
+    
+    @IBAction func buttonPressed() {
+        count+=1
+       scoreLabel.text = "Score \n\(count)"
+        NSLog("Button Pressed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupGame()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setupGame() {
+        seconds = 30
+        count = 0
+        
+        timeLabel.text = "Time: \(seconds)"
+        scoreLabel.text = "Score: \(count)"
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("subtractTime"), userInfo: nil, repeats: true)
+    }
+    
+    func subtractTime() {
+        seconds--
+        timeLabel.text = "Time: \(seconds)"
+        
+        if(seconds == 0) {
+            timer.invalidate()
+        }
+    }
 
 }
 
